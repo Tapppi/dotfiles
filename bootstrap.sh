20844c9765e5876bbecb2e3b5d1858e135258193
Tapani Moilanen <moilanen.tapani@gmail.com>
2019-02-19 02:17:05 +0200

    Exclude brew.sh from copy

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,7 @@ function doIt() {
 		--exclude ".osx" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
+		--exclude "brew.sh" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
