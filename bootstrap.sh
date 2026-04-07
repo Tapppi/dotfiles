66192d7f224c1d185060d4eddcf107f4917c3273
Tapani Moilanen <moilanen.tapani@gmail.com>
2026-04-07 15:21:06 +0300

    Shellcheck fixes for bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")"
+cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 
 git pull origin
 
-function doIt() {
+doIt() {
 	rsync --exclude ".git/" \
 		--exclude ".git" \
 		--exclude ".gitattributes" \
@@ -25,18 +25,17 @@ function doIt() {
 	mkdir -p ~/Library/Keyboard\ Layouts
 	cp -R keyboard-layouts/*.bundle ~/Library/Keyboard\ Layouts/
 
+	# shellcheck source=/dev/null
 	source ~/.bash_profile
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
+if [[ "$1" == "--force" ]] || [[ "$1" == "-f" ]]; then
 	doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	read -rp "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
 	echo ""
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
 		doIt
 	fi
 fi
 unset doIt
-
-cd - >/dev/null 2>&1
